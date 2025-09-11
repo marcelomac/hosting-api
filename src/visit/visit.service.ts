94da58aaf85cebe0bc516c7fff9d1669ecf58f71
Dev CMSJ <marcelo.a.mac@gmail.com>
2025-09-11 12:44:00 -0300

    20250911

--- src/visit/visit.service.ts
+++ src/visit/visit.service.ts
@@ -9,6 +9,7 @@ import { SysLogService } from 'src/syslog/syslog.service';
 import * as dotenv from 'dotenv';
 import { debugLog } from 'src/helpers/utils/debugLog';
 import { PersonService } from 'src/person/person.service';
+import { format } from 'date-fns';
 
 dotenv.config();
 @Injectable()
@@ -22,15 +23,30 @@ export class VisitService {
   ) {}
 
   async create(createVisitDto: CreateVisitDto) {
-    const fixedVisitDto = {
-      ...createVisitDto,
-      date: formatISO(parseISO(createVisitDto.date)),
-    };
-
     try {
-      return this.prisma.visit.create({
+      const fixedVisitDto = {
+        ...createVisitDto,
+        date: formatISO(parseISO(createVisitDto.date)),
+      };
+
+      const { id } = await this.prisma.visit.create({
         data: fixedVisitDto,
       });
+
+      const visit = await this.findVisitById(id);
+
+      await this.sysLogService.createSysLog({
+        action: 'Cadastro de visita',
+        message: `Data/hora do registro: ${visit.createdAt} |
+          Data/hora da visita: ${format(visit.date, 'dd/MM/yyyy')}-${visit.timeIn} |
+          Pessoa: ${visit.Person.name} | 
+          Destino: ${visit.Department.name} | 
+          Usuário: ${visit.User.id} - ${visit.User.name} |
+          visitId: ${visit.id}`,
+        file: 'visit',
+      });
+
+      return visit;
     } catch (error) {
       throw new Error(error);
     }
@@ -56,22 +72,40 @@ export class VisitService {
       include: {
         Person: true,
         Department: true,
+        User: true,
       },
     });
 
     return response;
   }
 
-  update(id: string, updateVisitDto: UpdateVisitDto) {
+  async update(id: string, updateVisitDto: UpdateVisitDto) {
     const fixedVisitDto = {
       ...updateVisitDto,
       date: formatISO(parseISO(updateVisitDto.date)),
     };
 
-    return this.prisma.visit.update({
-      where: { id },
-      data: fixedVisitDto,
-    });
+    try {
+      await this.prisma.visit.update({
+        where: { id },
+        data: fixedVisitDto,
+      });
+
+      const updatedVisit = await this.findVisitById(fixedVisitDto.id);
+
+      await this.sysLogService.createSysLog({
+        action: 'Alteração de visita',
+        message: `Data/hora do registro: ${updatedVisit.updatedAt} |
+          Data/hora da visita: ${format(updatedVisit.date, 'dd/MM/yyyy')}-${updatedVisit.timeIn} |
+          Pessoa: ${updatedVisit.Person.name} | 
+          Destino: ${updatedVisit.Department.name} | 
+          Usuário: ${updatedVisit.User.id} - ${updatedVisit.User.name} |
+          visitId: ${updatedVisit.id}`,
+        file: 'visit',
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   remove(id: string) {
